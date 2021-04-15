b58f79b547f3aed059a48fa5013032bc660ed6c1
jaym21 <jaynmistry21@gmail.com>
2021-04-15 11:07:43 +0530

    added start button and game activity

---
+++ app/src/main/java/dev/jaym/xno/Game.kt
@@ -0,0 +1,11 @@
+package dev.jaym.xno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
--- app/src/main/java/dev/jaym/xno/MainActivity.kt
+++ app/src/main/java/dev/jaym/xno/MainActivity.kt
@@ -1,11 +1,27 @@
 package dev.jaym.xno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.jaym.xno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnStart?.setOnClickListener {
+            startActivity(Intent(this, Game::class.java))
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
